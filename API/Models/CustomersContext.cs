13abb7c2e87d4ea76023dd62b1f95ffa67244ef8
AmilaDeAlwis <amilaalwis4@gmail.com>
2024-11-28 21:13:05 +0530

    using scafolding, models created.

---
+++ API/Models/CustomersContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Models;
+
+public partial class CustomersContext : DbContext
+{
+    public CustomersContext()
+    {
+    }
+
+    public CustomersContext(DbContextOptions<CustomersContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Address> Addresses { get; set; }
+
+    public virtual DbSet<Company> Companies { get; set; }
+
+    public virtual DbSet<Contact> Contacts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            throw new InvalidOperationException("The database connection must be configured using 'DbContextOptions'.");
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Address>(entity =>
+        {
+            entity.ToTable("Address");
+
+            entity.Property(e => e.AddressLine2).HasMaxLength(50);
+            entity.Property(e => e.City).HasMaxLength(100);
+            entity.Property(e => e.Country).HasMaxLength(100);
+            entity.Property(e => e.State).HasMaxLength(50);
+            entity.Property(e => e.StreetName).HasMaxLength(500);
+            entity.Property(e => e.StreetNumber).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Company>(entity =>
+        {
+            entity.ToTable("Company");
+
+            entity.Property(e => e.Name).HasMaxLength(100);
+
+            entity.HasOne(d => d.Address).WithMany(p => p.Companies)
+                .HasForeignKey(d => d.AddressId)
+                .HasConstraintName("FK_Company_Address");
+        });
+
+        modelBuilder.Entity<Contact>(entity =>
+        {
+            entity.ToTable("Contact");
+
+            entity.Property(e => e.Email).HasMaxLength(50);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+            entity.Property(e => e.MiddleName).HasMaxLength(50);
+            entity.Property(e => e.Phone).HasMaxLength(50);
+
+            entity.HasOne(d => d.Company).WithMany(p => p.Contacts)
+                .HasForeignKey(d => d.CompanyId)
+                .HasConstraintName("FK_Contact_Company");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
